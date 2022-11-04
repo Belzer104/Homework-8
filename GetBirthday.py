67b833029f3d2eb7f94a23c91ede35a1cb43c059
Belzer104 <shablovskyiillia@gmail.com>
2022-11-04 23:42:47 +0200

    Update GetBirthday.py

--- GetBirthday.py
+++ GetBirthday.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 
-def get_birthdays_per_week(list):
-    # user_input = input('Enter datetime in format dd.mm [-]: ')
-    for item in list:
+def get_birthdays_per_week(li):
+    l = list()
+    for item in li:
+        
         for key, value in item.items():
-            user_entered_date = datetime.strptime(value, '%d.%m')
+            birthday_date = datetime.strptime(value, '%d.%m')
             current_date = datetime.now()
-            user_entered_date = user_entered_date.replace(year=current_date.year)
-            if current_date > user_entered_date:
-                user_entered_date = user_entered_date.replace(year=current_date.year + 1)
-            delta = user_entered_date - current_date
-            return f'{delta.days} days left'
 
-print(get_birthdays_per_week([{"Jill": "07.11"}, {"Illia":"10.11"}, {"Jullia":"30.11"}, {"Ivan":"12.11"},{"Jeka":"30.10"},{"Jack":"08.11"}, {"Jisus":"09.11"}, {"Hulio":"11.11"}]))
+            birthday_date = birthday_date.replace(year=current_date.year)
+            if current_date > birthday_date:
+                birthday_date = birthday_date.replace(year=current_date.year + 1)
+            delta = birthday_date - current_date
+            if delta.days <= 7:
+                l.append(f"{birthday_date.strftime('%A')}: {item[0]}")
+    return l
+            
+            
+
+print(get_birthdays_per_week([{"Jill": "07.11"}, {"Illia":"07.11"}, {"Jullia":"30.11"}, {"Ivan":"12.11"},{"Jeka":"30.10"},{"Jack":"08.11"}, {"Jisus":"09.11"}, {"Hulio":"11.11"}]))
     
