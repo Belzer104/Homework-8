63b78a3dfa7ac18c49433fcfea615ab35232d2ab
Belzer104 <shablovskyiillia@gmail.com>
2022-11-04 22:41:46 +0200

    Update GetBirthday.py

--- GetBirthday.py
+++ GetBirthday.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 def get_birthdays_per_week():
-    pass
+    user_input = input('Enter datetime in format dd.mm [-]: ')
+    user_entered_date = datetime.strptime(user_input, '%d.%m')
+    print(user_entered_date)
+
+    
 
 
 if __name__ == '__main__':
