c9b47ff4256abfb32219ccccf30821974eea76dc
Belzer104 <shablovskyiillia@gmail.com>
2022-11-05 00:51:32 +0200

    Update GetBirthday.py

--- GetBirthday.py
+++ GetBirthday.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def get_birthdays_per_week(li):
-    l = list()
+    d = dict()
     for item in li:
         
         for key, value in item.items():
@@ -13,10 +13,12 @@ def get_birthdays_per_week(li):
                 birthday_date = birthday_date.replace(year=current_date.year + 1)
             delta = birthday_date - current_date
             if delta.days <= 7:
-                l.append(f"{birthday_date.strftime('%A')}: {item[0]}")
-    return l
+               d.setdefault(birthday_date.strftime('%A'),[]).append(key)
+    for k, v in d.items():
+        name = ", ".join(v)
+        print(f"{k}: {name}")
             
             
 
-print(get_birthdays_per_week([{"Jill": "07.11"}, {"Illia":"07.11"}, {"Jullia":"30.11"}, {"Ivan":"12.11"},{"Jeka":"30.10"},{"Jack":"08.11"}, {"Jisus":"09.11"}, {"Hulio":"11.11"}]))
-    
+get_birthdays_per_week([{"Jill": "07.11"}, {"Illia":"07.11"}, {"Jullia":"30.11"}, {"Ivan":"12.11"},{"Jeka":"30.10"},{"Jack":"08.11"}, {"Jisus":"09.11"}, {"Hulio":"11.11"}])
+  
